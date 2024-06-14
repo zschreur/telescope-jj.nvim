d0f40e4b504a09f828c39873cf384d3dd90bdf58
Zackary Schreur <z.schreur@techsmith.com>
2024-06-14 07:48:41 -0400

    Remove end of string match from conflict regex

--- lua/telescope-jj/conflicts.test.lua
+++ lua/telescope-jj/conflicts.test.lua
@@ -2,11 +2,11 @@ local s1 = "file 2-sided conflict"
 local s2 = "file  2-sided conflict"
 local s3 = "file   2-sided conflict"
 local s4 = "file   3-sided conflict"
-local s5 = "file 2-sided conflict 2-sided conflict"
+local s5 = "file 2-sided conflict including 1 deletion"
 
-local pattern = "^(.-)%s+%d+%-sided conflict$"
+local pattern = "^(.-)%s+%d+%-sided conflict"
 assert(string.match(s1, pattern) == "file")
 assert(string.match(s2, pattern) == "file")
 assert(string.match(s3, pattern) == "file")
 assert(string.match(s4, pattern) == "file")
-assert(string.match(s5, pattern) == "file 2-sided conflict")
+assert(string.match(s5, pattern) == "file")
