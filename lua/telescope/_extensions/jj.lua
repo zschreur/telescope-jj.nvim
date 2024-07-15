1f2080eeb78b7daa00f406f0d56eef4d7adaddcd
Zackary Schreur <z.schreur@techsmith.com>
2024-07-15 09:16:47 -0400

    Add healthcheck
    - Checks that jj is installed
    - Checks that jj version is supported

--- lua/telescope/_extensions/jj.lua
+++ lua/telescope/_extensions/jj.lua
@@ -9,4 +9,21 @@ return telescope.register_extension({
         -- access extension config and user config
     end,
     exports = require("telescope-jj"),
+    health = function()
+        if vim.fn.executable("jj") == 1 then
+            vim.health.ok("jj installed")
+        end
+
+        local obj = vim.system({ "jj", "--version" }, { text = true }):wait()
+        for major, minor, patch in string.gmatch(obj.stdout, "jj (%d+).(%d+).(%d+)") do
+            if not (tonumber(major) == 0 and tonumber(minor) >= 19) then
+                vim.health.warn("requires jj version 0.19.0 or greater")
+                vim.health.info("found jj version: " .. major .. "." .. minor .. "." .. patch)
+            else
+                vim.health.ok(
+                    "jj version " .. major .. "." .. minor .. "." .. patch .. " is supported"
+                )
+            end
+        end
+    end,
 })
